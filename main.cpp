0d8ead1eb799c766bd38697cfe4bd107fe918d7c
Andrei-Mihai Nicolae <nclandrei@users.noreply.github.com>
2014-05-07 05:03:47 +0200

    Updated main.cpp to a complete version
    Last update, it is fully functional now!!! Including new game and game over screens (fully customized and original work) and obstacles randomly generated.

--- main.cpp
+++ main.cpp
@@ -12,11 +12,15 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <termios.h>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 char buffer[10000];                                                          // buffer contains ASCII chars for the motorcycle
 int pos=0;                                                                   // current position of the motorcycle, updated in real time; it can go to +13 and -13, as the motorcycle is not allowed
+int z=7116;                                                                  // this is used for printing the obstacles (memorizing )
+char temporary[10000],game_over[10000],new_game[20000];
 
 /* void *printRoad(void *threadid){                                          // code can be modified to use the road and automobile as two separate objects( to switch, uncomment this section
                                                                              // and activate the third thread in main)         
@@ -41,12 +45,57 @@ int pos=0;                                                                   // 
 } */
 
 void *printCar(void *threadid){
+    srand(time(NULL));
     for(;;)
-       { 
-        cout << buffer;
-        usleep(500000);}
+       { for(z=rand()% 70 +7117;z>=67;z=z-150){
+        strcpy(temporary,buffer);
+        temporary[z] = '_';
+        temporary[z+1] = '_';
+        temporary[z+2] = '_';
+        temporary[z+3] = '_';
+        temporary[z+4] = '_';
+        temporary[z+5] = '_';
+        temporary[z+6] = '_';
+        temporary[z+7] = '_';
+        temporary[z+8] = '_';
+        temporary[z+9] = '_';
+        temporary[z+10] = '_';
+        temporary[z+11] = '_';
+        temporary[z+12] = '_';
+        temporary[z+13] = '_';
+        temporary[z+14] = '_';
+        temporary[z+15] = '_';
+        temporary[z+16] = '_';
+        temporary[z+17] = '_';
+        temporary[z+18] = '_';
+        temporary[z+19] = '_';
+        if(buffer[z] == '#' || buffer[z] == '0' || buffer[z] == '|' || buffer[z] == '-' || buffer[z] == '(' || buffer[z] == ')' || buffer[z] == '/' || buffer[z] == '=' || 
+           buffer[z+1] == '#' || buffer[z+1] == '0' || buffer[z+1] == '|' || buffer[z+1] == '-' || buffer[z+1] == '(' || buffer[z+1] == ')' || buffer[z+1] == '/' || buffer[z+1] == '=' ||
+           buffer[z+2] == '#' || buffer[z+2] == '0' || buffer[z+2] == '|' || buffer[z+2] == '-' || buffer[z+2] == '(' || buffer[z+2] == ')' || buffer[z+2] == '/' || buffer[z+2] == '=' ||
+           buffer[z+3] == '#' || buffer[z+3] == '0' || buffer[z+3] == '|' || buffer[z+3] == '-' || buffer[z+3] == '(' || buffer[z+3] == ')' || buffer[z+3] == '/' || buffer[z+3] == '=' ||
+           buffer[z+4] == '#' || buffer[z+4] == '0' || buffer[z+4] == '|' || buffer[z+4] == '-' || buffer[z+4] == '(' || buffer[z+4] == ')' || buffer[z+4] == '/' || buffer[z+4] == '=' ||
+           buffer[z+5] == '#' || buffer[z+5] == '0' || buffer[z+5] == '|' || buffer[z+5] == '-' || buffer[z+5] == '(' || buffer[z+5] == ')' || buffer[z+5] == '/' || buffer[z+5] == '=' ||
+           buffer[z+6] == '#' || buffer[z+6] == '0' || buffer[z+6] == '|' || buffer[z+6] == '-' || buffer[z+6] == '(' || buffer[z+6] == ')' || buffer[z+6] == '/' || buffer[z+6] == '=' ||
+           buffer[z+7] == '#' || buffer[z+7] == '0' || buffer[z+7] == '|' || buffer[z+7] == '-' || buffer[z+7] == '(' || buffer[z+7] == ')' || buffer[z+7] == '/' || buffer[z+7] == '=' ||
+           buffer[z+8] == '#' || buffer[z+8] == '0' || buffer[z+8] == '|' || buffer[z+8] == '-' || buffer[z+8] == '(' || buffer[z+8] == ')' || buffer[z+8] == '/' || buffer[z+8] == '=' ||
+           buffer[z+9] == '#' || buffer[z+9] == '0' || buffer[z+9] == '|' || buffer[z+9] == '-' || buffer[z+9] == '(' || buffer[z+9] == ')' || buffer[z+9] == '/' || buffer[z+9] == '=' ||
+           buffer[z+10] == '#' || buffer[z+10] == '0' || buffer[z+10] == '|' || buffer[z+10] == '-' || buffer[z+10] == '(' || buffer[z+10] == ')' || buffer[z+10] == '/' || buffer[z+10] == '=' || 
+           buffer[z+11] == '#' || buffer[z+11] == '0' || buffer[z+11] == '|' || buffer[z+11] == '-' || buffer[z+11] == '(' || buffer[z+11] == ')' || buffer[z+11] == '/' || buffer[z+10] == '=' ||
+           buffer[z+12] == '#' || buffer[z+12] == '0' || buffer[z+12] == '|' || buffer[z+12] == '-' || buffer[z+12] == '(' || buffer[z+12] == ')' || buffer[z+12] == '/' || buffer[z+10] == '=' ||
+           buffer[z+13] == '#' || buffer[z+13] == '0' || buffer[z+13] == '|' || buffer[z+13] == '-' || buffer[z+13] == '(' || buffer[z+13] == ')' || buffer[z+13] == '/' || buffer[z+10] == '=' ||
+           buffer[z+14] == '#' || buffer[z+14] == '0' || buffer[z+14] == '|' || buffer[z+14] == '-' || buffer[z+14] == '(' || buffer[z+14] == ')' || buffer[z+14] == '/' || buffer[z+10] == '=' ||
+           buffer[z+15] == '#' || buffer[z+15] == '0' || buffer[z+15] == '|' || buffer[z+15] == '-' || buffer[z+15] == '(' || buffer[z+15] == ')' || buffer[z+15] == '/' || buffer[z+10] == '=' ||
+           buffer[z+16] == '#' || buffer[z+16] == '0' || buffer[z+16] == '|' || buffer[z+16] == '-' || buffer[z+16] == '(' || buffer[z+16] == ')' || buffer[z+16] == '/' || buffer[z+10] == '=' ||
+           buffer[z+17] == '#' || buffer[z+17] == '0' || buffer[z+17] == '|' || buffer[z+17] == '-' || buffer[z+17] == '(' || buffer[z+17] == ')' || buffer[z+17] == '/' || buffer[z+10] == '=' ||
+           buffer[z+18] == '#' || buffer[z+18] == '0' || buffer[z+18] == '|' || buffer[z+18] == '-' || buffer[z+18] == '(' || buffer[z+18] == ')' || buffer[z+18] == '/' || buffer[z+10] == '=' ||
+           buffer[z+19] == '#' || buffer[z+19] == '0' || buffer[z+19] == '|' || buffer[z+19] == '-' || buffer[z+19] == '(' || buffer[z+19] == ')' || buffer[z+19] == '/' || buffer[z+10] == '=')   {
+              
+                cout << game_over; return 0; }
+        else {cout << temporary;
+             usleep(100000);} } }
     // cout << flush;
     pthread_exit(NULL);
+
 }
 
 void *readDir(void *threadid){
@@ -122,13 +171,54 @@ void *readDir(void *threadid){
     pthread_exit(NULL);   
 }
 
+/* void *printObst(void *threadid){
+  for(;;){
+    int random = rand()%7180 + 7117;
+    buffer[random] = '-';
+    buffer[random+1] = '-';
+    buffer[random+2] = '-';
+    buffer[random+3] = '-';
+    buffer[random+4] = '-';
+    buffer[random+5] = '-';
+    . . . .
 
+  }
+  pthread_exit(NULL);
+} */
 
 int main(){
     pthread_t threads[3];
-
+    srand(time(NULL));
     char ch;
-    int i=0,rc;
+    char choice;                        // choice for the game (n for new game, q for quit)
+    int i = 0;
+
+    ifstream gameover;
+    gameover.open("game_over.txt");
+    while(!gameover.eof()){
+      gameover.get(ch);
+      game_over[i] = ch;
+      i++;
+    }
+    game_over[strlen(game_over)-1] = '\0';
+    gameover.close();
+    i=0;
+    if (system("CLS")) system("clear");
+
+    ifstream newgame;
+    newgame.open("new_game.txt");
+    while(!newgame.eof()){
+      newgame.get(ch);
+      new_game[i] = ch;
+      i++;
+    }
+    new_game[strlen(new_game)-1] = '\0';
+    newgame.close();
+    cout << new_game;
+    cin >> choice;
+    if( choice == 'n'){
+    i=0;
+    int rc;
 
     ifstream myfile;                    // opening a file text with the ASCII drawing of the automobile used
     myfile.open("bike.txt");
@@ -143,11 +233,6 @@ int main(){
     // buffer[strlen(buffer)-2] = '\0';
     myfile.close();
 
-        /* rc = pthread_create(&threads[0], NULL, printRoad, (void *)0);
-              if (rc){
-                 cout << "Error:unable to create thread," << rc << endl;
-                 exit(-1);
-              }*/
         rc = pthread_create(&threads[1], NULL, readDir, (void *)1);
               if (rc){
                 cout << "Error:unable to create thread," << rc << endl;
@@ -160,8 +245,14 @@ int main(){
                 exit(-1);
               }
 
+        /* rc = pthread_create(&threads[2], NULL, printObst, (void *)2);
+              if (rc){
+                 cout << "Error:unable to create thread," << rc << endl;
+                 exit(-1);
+              } */
+
     pthread_exit(NULL);
     myfile.close();
-
-    return 0;
+   }
+   else return 0;
 }
